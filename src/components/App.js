15d63d0173d348f1d760e0a6cf4645ec28bf8571
robert-cummins <robert.k.cummins@gmail.com>
2020-03-27 16:50:17 +1300

    Save data returned asynchronously in a variable

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,15 @@ import SearchBar from './SearchBar'
 import axios from 'axios'
 
 class App extends React.Component {
-    onSearchSubmit(term){
-        axios.get('https://api.unsplash.com/search/photos', {
-            params: {query: term},
+    async onSearchSubmit(term){
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term}, 
             headers: {
                 Authorization: 'Client-ID dXgb1yXvlV4a0E_TR_8Ehgg4yQS53I_hey7y-pEtbPY' 
             }
         })
+
+        console.log(response.data.results)
     }
 
     render() {
